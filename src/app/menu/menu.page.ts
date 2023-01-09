8dcb1e8480b037facb30b9cadb03e22eea26bd0e
raulbenavides27 <ra.benavides@duocuc.cl>
2023-01-09 20:45:25 -0300

    act

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  indiceSeleccionado: number = 0;
+
+  paginas = [
+    {
+      titulo: 'Home',
+      url: '/menu/home',
+      icono: 'home'
+    },
+    {
+      titulo: 'Crear lista',
+      url: '/menu/crearlista',
+      icono: 'receipt'
+      
+    },
+    {
+      titulo: 'Notas',
+      url: '/menu/mnotas',
+      icono: 'book'
+    },
+    {
+      titulo: 'Ejemplo API',
+      url: '/menu/pendientes',
+      icono: 'book'
+    },
+    {
+      titulo: 'Favoritos',
+      url: '/menu/favorito',
+      icono: 'book'
+    }
+  
+  ]
+
+  constructor(public alertController: AlertController,
+    public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+ // cambiarIndiceSeleccionado(i){
+   // this.indiceSeleccionado = i;
+  //}
+
+  async salir(){
+    const alert = await this.alertController.create({
+      header: 'Salir',
+      message: '¿Confirmar para salir?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            
+          }
+        }, {
+          text: 'Aceptar',
+          handler: () => {
+            localStorage.removeItem('ingresado');
+            this.navCtrl.navigateRoot('login');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 }
+
